471ba7d8cbf73a51f2e36b26ab1d9382e9789ff2
hong <gmm117@naver.com>
2022-04-17 23:13:46 +0900

    add project page

---
+++ pages/project.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import { Container, Title } from "../styles/Core";
+
+const Project: NextPage = () => {
+	return (
+		<Container>
+			<Title>Project</Title>
+		</Container>
+	);
+};
+
+export default Project;
