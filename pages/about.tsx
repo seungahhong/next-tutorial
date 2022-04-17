f759b8c71537868a10793afc7ac9377704130154
hong <gmm117@naver.com>
2022-04-17 22:54:43 +0900

    add nextjs emotion eslint prettier

---
+++ pages/about.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import { Container, Title } from "../styles/Core";
+
+const About: NextPage = () => {
+	return (
+		<Container>
+			<Title>about</Title>
+		</Container>
+	);
+};
+
+export default About;
---
+++ styles/Core.tsx
@@ -0,0 +1,15 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	flex: 1;
+	height: 100%;
+	margin: 0 auto;
+`;
+
+export const Title = styled.div`
+	font-size: 100px;
+	font-weight: 700;
+`;
